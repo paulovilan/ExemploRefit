10b77bd9fcac598c4e0d3571310d6ca2d13ab31d
Paulo Vilan <paulovilan@gmail.com>
2021-07-20 11:10:11 -0300

    Adicionar arquivos de projeto.

---
+++ ExemploRefit/GetAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ExemploRefit
+{
+    internal class GetAttribute : Attribute
+    {
+        private string v;
+
+        public GetAttribute(string v)
+        {
+            this.v = v;
+        }
+    }
+}
---
+++ ExemploRefit/ICepApiService.cs
@@ -0,0 +1,12 @@
+﻿using Refit;
+using System.Threading.Tasks;
+
+
+namespace ExemploRefit
+{
+	public interface ICepApiService
+	{
+		[Get("/ws/{cep}/json")]
+		Task<CepResponse> GetAddressAsync(string cep);
+	}
+}
